1c9b9e1206702f3a5771f3baac2863e2560defaa
Jeremy Zhang <41759551+jeremyzhang1@users.noreply.github.com>
2019-09-25 22:47:37 -0500

    Add files via upload

---
+++ judge.bat
@@ -0,0 +1,29 @@
+@echo off
+cls
+call classpath.bat
+echo Compile in progress
+:compile
+javac Solution.java
+echo Compile Successful? (Y/N)
+set /p "r1=>"
+if %r1% == N goto fail
+if %r1% == n goto fail
+if %r1% == Y goto run
+:run
+java Solution < judgein.txt > output.txt
+echo Runtime Successful? (Y/N)
+set /p "r2=>"
+if %r2% == N goto fail
+if %r2% == n goto fail
+if %r2% == Y goto diff
+:diff
+fc judgeout.txt output.txt
+pause
+goto end
+:fail
+echo Submission failed
+goto end
+:end
+echo Quitting program
+pause
+exit
