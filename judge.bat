d3dbd6cd947b4c6047b5b211a3db9003d504b582
John (JB) Ladera <domobaby10@gmail.com>
2019-10-03 18:54:16 -0500

    Added support for "y" as a response for yes
    Also added spacing between sections to make the code more readable

--- judge.bat
+++ judge.bat
@@ -2,6 +2,7 @@
 cls
 call classpath.bat
 echo Compile in progress
+
 :compile
 javac Solution.java
 echo Compile Successful? (Y/N)
@@ -9,6 +10,8 @@ set /p "r1=>"
 if %r1% == N goto fail
 if %r1% == n goto fail
 if %r1% == Y goto run
+if %r1% == y goto run
+
 :run
 java Solution < judgein.txt > output.txt
 echo Runtime Successful? (Y/N)
@@ -16,13 +19,17 @@ set /p "r2=>"
 if %r2% == N goto fail
 if %r2% == n goto fail
 if %r2% == Y goto diff
+if %r2% == y goto diff
+
 :diff
 fc judgeout.txt output.txt
 pause
 goto end
+
 :fail
 echo Submission failed
 goto end
+
 :end
 echo Quitting program
 pause
